a37a332d2fe877ed108368c0c8e43a556868060c
devneopark <dev.neopark@gmail.com>
2026-07-19 20:55:47 +0900

    build: Room 도메인서비스 모듈 추가

---
+++ backend/modules/libs/domains/room/service/build.gradle.kts
@@ -0,0 +1,7 @@
+version = "0.0.1"
+
+dependencies {
+    implementation("com.devneopark.chat.backend:shared-kernel:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:shared-domain-exception:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-room-reference:0.0.2-SNAPSHOT")
+}
