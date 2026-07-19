37602fc7abf76368ceda71da59e3f2a8f4dca915
devneopark <dev.neopark@gmail.com>
2026-07-19 20:24:11 +0900

    build: User 도메인 모델 모듈 추가

---
+++ backend/modules/libs/domains/user/model/build.gradle.kts
@@ -0,0 +1,6 @@
+version = "0.0.1"
+
+dependencies {
+    implementation("com.devneopark.chat.backend:shared-domain-exception:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-user-reference:0.0.1-SNAPSHOT")
+}
