4f11ebd21a9e7aa4b318866cea3d0c594f0731d6
devneopark <dev.neopark@gmail.com>
2026-07-19 21:47:30 +0900

    build: Chat Message 도메인 모델 모듈 추가

---
+++ backend/modules/libs/domains/chat-message/model/build.gradle.kts
@@ -0,0 +1,9 @@
+version = "0.0.1"
+
+dependencies {
+    implementation("com.devneopark.chat.backend:shared-kernel:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:shared-domain-exception:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-chat-message-reference:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-participant-reference:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-room-reference:0.0.1-SNAPSHOT")
+}
