3a0ebdcb38d3cc1546c0e14a09f89ed8b2483c4c
devneopark <dev.neopark@gmail.com>
2026-08-04 21:30:54 +0900

    build: Authentication Grant 도메인 모델 모듈 추가

---
+++ backend/modules/libs/domains/authentication-grant/model/build.gradle.kts
@@ -0,0 +1,8 @@
+version = "0.0.1"
+
+dependencies {
+    implementation("com.devneopark.chat.backend:shared-kernel:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:shared-domain-exception:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-user-reference:0.0.1-SNAPSHOT")
+    implementation("com.devneopark.chat.backend:domain-authentication-grant-reference:0.0.1-SNAPSHOT")
+}
